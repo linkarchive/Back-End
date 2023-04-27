9b10bcb0d40a4eb929c02e59fddd28c5469ba87e
taegyun1995 <needle12s@gmail.com>
2023-04-27 11:21:43 +0900

    feat: project setting

---
+++ src/main/java/project/linkarchive/backend/LinkarchiveApplication.java
@@ -0,0 +1,13 @@
+package project.linkarchive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkarchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/linkarchive/backend/LinkarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package project.linkarchive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkarchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
