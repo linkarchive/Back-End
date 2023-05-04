061be54ad4076249e238709a0c7b1758e1570828
taegyun1995 <needle12s@gmail.com>
2023-05-04 21:06:06 +0900

    fix: success code const modify

--- src/main/java/project/linkarchive/backend/advice/success/SuccessCodeConst.java
+++ src/main/java/project/linkarchive/backend/advice/success/SuccessCodeConst.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum SuccessCodeConst {
 
-    URL_CREATE(201, "PLAN_CREATE", "url 이 생성되었습니다.");
+    URL_CREATE(201, "URL_CREATE", "url 이 생성되었습니다.");
 
     private int status;
     private String code;
