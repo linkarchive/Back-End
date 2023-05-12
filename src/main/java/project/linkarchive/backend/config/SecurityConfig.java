7c02b98a55a16a5b7370a24172a9439beff0d128
subin <lsb1026@naver.com>
2023-05-12 17:15:02 +0900

    feat: token에서userId 리턴해주는 ArgumentResolver 생성

--- src/main/java/project/linkarchive/backend/config/SecurityConfig.java
+++ src/main/java/project/linkarchive/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package project.linkarchive.backend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,13 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.linkarchive.backend.auth.service.OAuthService;
 import project.linkarchive.backend.security.AuthenticationEntryPointImpl;
+import project.linkarchive.backend.security.SecurityArgumentResolver;
 import project.linkarchive.backend.security.TokenAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
-public class SecurityConfig {
+@RequiredArgsConstructor
+public class SecurityConfig implements WebMvcConfigurer {
+    private final SecurityArgumentResolver securityArgumentResolver;
 
     @Bean
     public SecurityFilterChain setupSecurity(
@@ -30,6 +38,10 @@ public class SecurityConfig {
                 .authenticationEntryPoint(new AuthenticationEntryPointImpl())
                 .and()
                 .build();
+    }
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(securityArgumentResolver);
     }
 
 }
