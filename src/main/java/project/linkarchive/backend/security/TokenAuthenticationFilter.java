432be7b54726e230f35174096a54287cf3ee1a31
subin <lsb1026@naver.com>
2023-05-13 15:41:16 +0900

    fix: tokenData.length != 1 -> 2

--- src/main/java/project/linkarchive/backend/security/TokenAuthenticationFilter.java
+++ src/main/java/project/linkarchive/backend/security/TokenAuthenticationFilter.java
@@ -30,7 +30,7 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-        if (tokenData.length != 1) {
+        if (tokenData.length != 2) {
             filterChain.doFilter(request, response);
             return;
         }
