66f9ce60a3c1a95aceec916aaa3ccf3ff020f71c
taegyun1995 <needle12s@gmail.com>
2023-05-15 12:03:05 +0900

    fix: link list query exclude login user

--- src/main/java/project/linkarchive/backend/url/service/UrlQueryService.java
+++ src/main/java/project/linkarchive/backend/url/service/UrlQueryService.java
@@ -1,6 +1,5 @@
 package project.linkarchive.backend.url.service;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,13 +10,10 @@ import project.linkarchive.backend.url.repository.UrlRepositoryImpl;
 import project.linkarchive.backend.url.response.linkList.UserExcludedLinkListDetailResponse;
 import project.linkarchive.backend.url.response.linkList.UserExcludedLinkListResponse;
 import project.linkarchive.backend.url.response.linkList.UserExcludedLinkTagListDetailResponse;
-import project.linkarchive.backend.url.response.linkList.UserExcludedTagListDetailResponse;
 import project.linkarchive.backend.url.response.userLinkList.UserLinkListDetailResponse;
 import project.linkarchive.backend.url.response.userLinkList.UserLinkListResponse;
 import project.linkarchive.backend.url.response.userLinkList.UserLinkTagListDetailResponse;
 import project.linkarchive.backend.url.response.userLinkList.UserTagListDetailResponse;
-import project.linkarchive.backend.user.domain.UserHashTag;
-import project.linkarchive.backend.user.repository.UserHashTagRepository;
 import project.linkarchive.backend.user.repository.UserHashTagRepositoryImpl;
 
 import java.util.List;
@@ -27,13 +23,11 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class UrlQueryService {
 
-    private final UserHashTagRepository userHashTagRepository;
     private final UrlHashTagRepository urlHashTagRepository;
     private final UrlRepositoryImpl urlRepositoryImpl;
     private final UserHashTagRepositoryImpl userHashTagRepositoryImpl;
 
-    public UrlQueryService(UserHashTagRepository userHashTagRepository, UrlHashTagRepository urlHashTagRepository, UrlRepositoryImpl urlRepositoryImpl, UserHashTagRepositoryImpl userHashTagRepositoryImpl) {
-        this.userHashTagRepository = userHashTagRepository;
+    public UrlQueryService(UrlHashTagRepository urlHashTagRepository, UrlRepositoryImpl urlRepositoryImpl, UserHashTagRepositoryImpl userHashTagRepositoryImpl) {
         this.urlHashTagRepository = urlHashTagRepository;
         this.urlRepositoryImpl = urlRepositoryImpl;
         this.userHashTagRepositoryImpl = userHashTagRepositoryImpl;
@@ -43,10 +37,10 @@ public class UrlQueryService {
         List<TagListDetailResponse> userHashTagList = userHashTagRepositoryImpl.getUserHashTagList(userId);
 
         //FIXME 기능에 초점을 둬서 쿼리 성능이 좋지 않아요.
-        List<UserLinkListDetailResponse> userUrlLinkListDetailResponseList = urlRepositoryImpl.getUserLinkList(pageable, lastUrlId);
-        List<UserLinkTagListDetailResponse> userLinkTagListDetailResponseList = userUrlLinkListDetailResponseList.stream()
-                .map(link -> {
-                    List<UrlHashTag> urlHashTagList = urlHashTagRepository.findByUrlId(link.getUrlId());
+        List<UserLinkListDetailResponse> linkList = urlRepositoryImpl.getUserLinkList(pageable, lastUrlId);
+        List<UserLinkTagListDetailResponse> userLinkTagListDetailResponseList = linkList.stream()
+                .map(url -> {
+                    List<UrlHashTag> urlHashTagList = urlHashTagRepository.findByUrlId(url.getUrlId());
                     List<UserTagListDetailResponse> userTagListDetailResponseList = urlHashTagList.stream()
                             .map(urlHashTag -> new UserTagListDetailResponse(
                                     urlHashTag.getHashTag().getId(),
@@ -54,12 +48,12 @@ public class UrlQueryService {
                             .collect(Collectors.toList());
 
                     return new UserLinkTagListDetailResponse(
-                            link.getUrlId(),
-                            link.getLink(),
-                            link.getTitle(),
-                            link.getDescription(),
-                            link.getThumbnail(),
-                            link.getBookMarkCount(),
+                            url.getUrlId(),
+                            url.getLink(),
+                            url.getTitle(),
+                            url.getDescription(),
+                            url.getThumbnail(),
+                            url.getBookMarkCount(),
                             userTagListDetailResponseList
                     );
                 }).collect(Collectors.toList());
@@ -67,30 +61,16 @@ public class UrlQueryService {
         return new UserLinkListResponse(userHashTagList, userLinkTagListDetailResponseList);
     }
 
-    public UserExcludedLinkListResponse getLinkList(Pageable pageable, Long lastUrlId) {
+    public UserExcludedLinkListResponse getLinkList(Pageable pageable, Long lastUrlId, Long userId) {
         //FIXME 기능에 초점을 둬서 쿼리 성능이 좋지 않아요.
-        //FIXME 로그인한 유저의 리스트를 제외하고 조회해야 해요.
-        List<UserExcludedLinkListDetailResponse> userExcludedLinkListDetailResponseList = urlRepositoryImpl.getLinkList(pageable, lastUrlId);
-        List<UserExcludedLinkTagListDetailResponse> userExcludedLinkTagListDetailResponseList = userExcludedLinkListDetailResponseList.stream()
-                .map(l -> {
-                    List<UrlHashTag> urlHashTagList = urlHashTagRepository.findByUrlId(l.getUrlId());
-                    List<UserExcludedTagListDetailResponse> userExcludedTagListDetailResponseList = urlHashTagList.stream()
-                            .map(h -> new UserExcludedTagListDetailResponse(
-                                    h.getHashTag().getId(),
-                                    h.getHashTag().getTag()))
+        List<UserExcludedLinkListDetailResponse> linkList = urlRepositoryImpl.getLinkList(pageable, lastUrlId, userId);
+        List<UserExcludedLinkTagListDetailResponse> userExcludedLinkTagListDetailResponseList = linkList.stream()
+                .map(url -> {
+                    List<UrlHashTag> urlHashTagList = urlHashTagRepository.findByUrlId(url.getUrlId());
+                    List<TagListDetailResponse> tagListDetailResponseList = urlHashTagList.stream()
+                            .map(h -> TagListDetailResponse.of(h.getHashTag().getTag()))
                             .collect(Collectors.toList());
-                    return new UserExcludedLinkTagListDetailResponse(
-                            l.getUserId(),
-                            l.getName(),
-                            l.getProfileImage(),
-                            l.getUrlId(),
-                            l.getLink(),
-                            l.getTitle(),
-                            l.getDescription(),
-                            l.getThumbnail(),
-                            l.getBookMarkCount(),
-                            userExcludedTagListDetailResponseList
-                    );
+                    return UserExcludedLinkTagListDetailResponse.of(url, tagListDetailResponseList);
                 }).collect(Collectors.toList());
 
         return new UserExcludedLinkListResponse(userExcludedLinkTagListDetailResponseList);
