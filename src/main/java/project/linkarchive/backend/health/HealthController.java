c73dabf4a8629a70e0c5226dc215feb47174b83b
taegyun1995 <needle12s@gmail.com>
2023-05-15 20:37:56 +0900

    test

--- src/main/java/project/linkarchive/backend/health/HealthController.java
+++ src/main/java/project/linkarchive/backend/health/HealthController.java
@@ -1,14 +1,17 @@
 package project.linkarchive.backend.health;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthController {
 
     @GetMapping("/health")
     public String health() {
+        log.info("ok");
         return "ok";
     }
-    
+
 }
