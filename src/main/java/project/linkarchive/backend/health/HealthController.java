3aba9bb120faaac7ff4c78b074a421c6c562aee6
taegyun1995 <needle12s@gmail.com>
2023-05-15 23:06:08 +0900

    feat: health check

--- src/main/java/project/linkarchive/backend/health/HealthController.java
+++ src/main/java/project/linkarchive/backend/health/HealthController.java
@@ -1,16 +1,16 @@
 package project.linkarchive.backend.health;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
+@PreAuthorize("isAuthenticated() == false")
 public class HealthController {
 
     @GetMapping("/health")
     public String health() {
-        log.info("ok");
         return "ok";
     }
 
