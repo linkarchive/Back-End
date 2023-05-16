84481d2415f708bcdeb5ebb8aa05459c1e5b1bab
taegyun1995 <needle12s@gmail.com>
2023-05-16 18:54:35 +0900

    fix: syntax error

--- src/main/java/project/linkarchive/backend/hashtag/service/HashTagApiService.java
+++ src/main/java/project/linkarchive/backend/hashtag/service/HashTagApiService.java
@@ -32,8 +32,7 @@ public class HashTagApiService {
         User user = findUserById(userId);
 
         HashTag hashTag = hashTagRepository.findByTag(request.getTag())
-                .orElseGet(() -> HashTag.bui
-                        ld(request));
+                .orElseGet(() -> HashTag.build(request));
 
         userHashTagRepository.findByHashTagId(hashTag.getId())
                 .ifPresentOrElse(userHashTag -> {
