4e1228b9e77cfc57053c3c08dfea933296613582
taegyun1995 <needle12s@gmail.com>
2023-05-16 18:35:22 +0900

    delete: url entity -> link entity

--- src/main/java/project/linkarchive/backend/url/repository/UrlRepository.java
+++
@@ -1,7 +0,0 @@
-package project.linkarchive.backend.url.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.linkarchive.backend.url.domain.Url;
-public interface UrlRepository extends JpaRepository<Url, Long> {
-
-}
