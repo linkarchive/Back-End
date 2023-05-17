e4a3af9ec2dd8ad46e04072867d770d488682a58
taegyun1995 <needle12s@gmail.com>
2023-05-17 21:42:53 +0900

    tmp

--- src/main/java/project/linkarchive/backend/config/WebConfig.java
+++ src/main/java/project/linkarchive/backend/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+                        "*",
                         "http://localhost:3000",
                         "https://front-end-git-dev-link-archive.vercel.app",
                         "https://front-end-git-test-api-https-link-archive.vercel.app")
