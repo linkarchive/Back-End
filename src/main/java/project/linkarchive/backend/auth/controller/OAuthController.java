7080b45214637761088122ed45bfdef9cadb20a8
taegyun1995 <needle12s@gmail.com>
2023-05-18 14:04:53 +0900

    fix: receive redirect uri with flwxibility

--- src/main/java/project/linkarchive/backend/auth/controller/OAuthController.java
+++ src/main/java/project/linkarchive/backend/auth/controller/OAuthController.java
@@ -8,6 +8,8 @@ import project.linkarchive.backend.auth.response.LoginResponse;
 import project.linkarchive.backend.auth.service.OAuthService;
 import project.linkarchive.backend.jwt.JwtProperties;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class OAuthController {
 
@@ -18,8 +20,11 @@ public class OAuthController {
     }
 
     @PostMapping("/auth/kakao")
-    public ResponseEntity<LoginResponse> login(@RequestParam("code") String code) {
-        String accessToken = oAuthService.login(code);
+    public ResponseEntity<LoginResponse> login(@RequestParam("code") String code, HttpServletRequest request) {
+        // Referer는 HTTP 헤더 중 하나로, 현재 요청을 보내는 페이지의 주소 URL이에요. *참고*
+        String referer = request.getHeader("Referer");
+        String redirectUri = referer + "auth/kakao";
+        String accessToken = oAuthService.login(code, redirectUri);
 
         return ResponseEntity.ok()
                 .header(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + accessToken)
