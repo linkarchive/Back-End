0c1a18c78bc7ae421fa72b7cfe63b194e89300af
taegyun1995 <needle12s@gmail.com>
2023-05-19 11:45:39 +0900

    feat: get tag quantity adjustments for marked links by user

--- src/main/java/project/linkarchive/backend/bookmark/controller/BookMarkQueryController.java
+++ src/main/java/project/linkarchive/backend/bookmark/controller/BookMarkQueryController.java
@@ -4,12 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.linkarchive.backend.bookmark.service.BookMarkQueryService;
 import project.linkarchive.backend.hashtag.response.TagListResponse;
-import project.linkarchive.backend.hashtag.response.TagResponse;
-
-import java.util.List;
 
 @RestController
 @PreAuthorize("isAuthenticated()")
@@ -21,10 +19,19 @@ public class BookMarkQueryController {
         this.bookMarkQueryService = bookMarkQueryService;
     }
 
+    // 사용자 별 북마크 리스트 중 해시태그 리스트 조회 011
     @GetMapping("/mark/tags/user/{userId}")
     public ResponseEntity<TagListResponse> getMarkTagList(@PathVariable("userId") Long userId) {
-        List<TagResponse> tagList = bookMarkQueryService.getMarkTagList(userId);
-        return ResponseEntity.ok(new TagListResponse(tagList));
+        TagListResponse tagList = bookMarkQueryService.getMarkTagList(userId);
+        return ResponseEntity.ok(tagList);
+    }
+
+    // 사용자 별 북마크 리스트 중 해시태그 N개 조회 009
+    @GetMapping("/mark/tags/limit/user/{userId}")
+    public ResponseEntity<TagListResponse> getMarkTagLimitList(@PathVariable("userId") Long userId,
+                                                               @RequestParam("size") Long size) {
+        TagListResponse tagList = bookMarkQueryService.getMarkTagLimitList(userId, size);
+        return ResponseEntity.ok(tagList);
     }
 
 }
