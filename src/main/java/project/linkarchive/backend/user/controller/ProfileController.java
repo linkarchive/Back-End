d6c0d69efb0365f4ae95102649ceab27210a4515
taegyun1995 <needle12s@gmail.com>
2023-05-20 17:39:54 +0900

    fix: optimize profile image editing methods

--- src/main/java/project/linkarchive/backend/user/controller/ProfileController.java
+++ src/main/java/project/linkarchive/backend/user/controller/ProfileController.java
@@ -1,6 +1,5 @@
 package project.linkarchive.backend.user.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,25 +8,31 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import project.linkarchive.backend.advice.success.SuccessCodeConst;
 import project.linkarchive.backend.advice.success.SuccessResponse;
 import project.linkarchive.backend.security.AuthInfo;
-import project.linkarchive.backend.user.service.ProfileService;
+import project.linkarchive.backend.user.service.ProfileImageService;
 
 import java.io.IOException;
 
+import static project.linkarchive.backend.advice.success.SuccessCodeConst.UPDATE_PROFILE_IMAGE;
+
 @RestController
-@RequiredArgsConstructor
 @PreAuthorize("isAuthenticated()")
 public class ProfileController {
-    private final ProfileService profileService;
+
+    private final ProfileImageService profileImageService;
+
+    public ProfileController(ProfileImageService profileImageService) {
+        this.profileImageService = profileImageService;
+    }
 
     @PatchMapping(value = "/profile-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<SuccessResponse> updateProfileImage(
-            AuthInfo authInfo,
-            @RequestParam(value = "image") MultipartFile image
+            @RequestParam(value = "image") MultipartFile image,
+            AuthInfo authInfo
     ) throws IOException {
-        profileService.saveProfileImage(image, authInfo.getId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(new SuccessResponse(SuccessCodeConst.UPDATE_PROFILE_IMAGE));
+        profileImageService.saveProfileImage(image, authInfo.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(new SuccessResponse(UPDATE_PROFILE_IMAGE));
     }
+
 }
