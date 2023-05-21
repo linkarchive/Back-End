bd15728fbf48396aa750dc87bd9d7c4838b17d1c
taegyun1995 <needle12s@gmail.com>
2023-05-21 18:29:09 +0900

    feat: cycle to the user's introduce empty array

--- src/main/java/project/linkarchive/backend/user/service/UserApiService.java
+++ src/main/java/project/linkarchive/backend/user/service/UserApiService.java
@@ -1,4 +1,49 @@
 package project.linkarchive.backend.user.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import project.linkarchive.backend.advice.exception.custom.NotFoundException;
+import project.linkarchive.backend.s3.S3Uploader;
+import project.linkarchive.backend.user.domain.ProfileImage;
+import project.linkarchive.backend.user.repository.UserProfileImageRepository;
+import project.linkarchive.backend.user.repository.UserRepository;
+
+import java.io.IOException;
+
+import static project.linkarchive.backend.advice.exception.ExceptionCodeConst.NOT_FOUND_PROFILE_IMAGE;
+import static project.linkarchive.backend.advice.exception.ExceptionCodeConst.NOT_FOUND_USER;
+
+@Service
+@Transactional
 public class UserApiService {
+
+    private final S3Uploader s3Uploader;
+    private final UserRepository userRepository;
+    private final UserProfileImageRepository userProfileImageRepository;
+
+    public UserApiService(S3Uploader s3Uploader, UserRepository userRepository, UserProfileImageRepository userProfileImageRepository) {
+        this.s3Uploader = s3Uploader;
+        this.userRepository = userRepository;
+        this.userProfileImageRepository = userProfileImageRepository;
+    }
+
+    public void saveProfileImage(MultipartFile image, Long userId) throws IOException {
+        validateUserExists(userId);
+        ProfileImage profileImage = findProfileImageByUserId(userId);
+
+        String storedFileName = s3Uploader.upload(image);
+        profileImage.updateProfileImage(storedFileName);
+    }
+
+    private void validateUserExists(Long userId) {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_USER));
+    }
+
+    private ProfileImage findProfileImageByUserId(Long userId) {
+        return userProfileImageRepository.findByUserId(userId)
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_PROFILE_IMAGE));
+    }
+
 }
