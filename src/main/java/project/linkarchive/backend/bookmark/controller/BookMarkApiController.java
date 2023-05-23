17f3b57aca8c1c6738cef0e668950e33f7e5a67f
subin <lsb1026@naver.com>
2023-05-23 16:48:21 +0900

    fix: import 정리

--- src/main/java/project/linkarchive/backend/bookmark/controller/BookMarkApiController.java
+++ src/main/java/project/linkarchive/backend/bookmark/controller/BookMarkApiController.java
@@ -2,7 +2,6 @@ package project.linkarchive.backend.bookmark.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/project/linkarchive/backend/link/controller/LinkQueryController.java
+++ src/main/java/project/linkarchive/backend/link/controller/LinkQueryController.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -72,7 +71,7 @@ public class LinkQueryController {
     }
 
     // 내 링크 리스트 조회 - 로그인 필요 O
-    @GetMapping("/links/user/")
+    @GetMapping("/links/user")
     public ResponseEntity<UserLinkListResponse> getUserLinkList(
             AuthInfo authInfo,
             @RequestParam(value = "linkId", required = false) Long lastLinkId,
