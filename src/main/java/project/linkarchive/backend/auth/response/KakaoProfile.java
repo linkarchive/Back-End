bffbc39a9d0ab2940743da62df483b6cc2d1922f
taegyun1995 <needle12s@gmail.com>
2023-05-31 16:40:19 +0900

    feat: modify logic around user domain test code

--- src/main/java/project/linkarchive/backend/auth/response/KakaoProfile.java
+++ src/main/java/project/linkarchive/backend/auth/response/KakaoProfile.java
@@ -2,9 +2,12 @@ package project.linkarchive.backend.auth.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoProfile {
 
@@ -20,6 +23,12 @@ public class KakaoProfile {
         @JsonSetter("email")
         public String email;
 
+    }
+
+    public KakaoProfile(String id, String email) {
+        this.id = id;
+        this.kakaoAccount = new KakaoAccount();
+        this.kakaoAccount.email = email;
     }
 
 }
