b7a9a210e7130baa15d18610ebaa6141d613dee1
taegyun1995 <needle12s@gmail.com>
2023-06-01 16:42:28 +0900

    test: repository transactional add

--- src/test/java/project/linkarchive/backend/util/RepositoryTest.java
+++ src/test/java/project/linkarchive/backend/util/RepositoryTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 import project.linkarchive.backend.user.repository.UserRepository;
 import project.linkarchive.backend.util.constant.SetUpData;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
 public class RepositoryTest extends SetUpData {
 
     @Autowired
