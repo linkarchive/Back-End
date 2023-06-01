e9896ca4dd5ec6f1272377db8537b5a7b0f0609d
taegyun1995 <needle12s@gmail.com>
2023-06-01 16:22:46 +0900

    test: user repository code

---
+++ src/test/java/project/linkarchive/backend/util/RepositoryTest.java
@@ -0,0 +1,22 @@
+package project.linkarchive.backend.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import project.linkarchive.backend.user.repository.UserRepository;
+import project.linkarchive.backend.util.constant.SetUpData;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RepositoryTest extends SetUpData {
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    @AfterEach
+    void cleanUp() {
+        userRepository.delete(user);
+    }
+
+}
