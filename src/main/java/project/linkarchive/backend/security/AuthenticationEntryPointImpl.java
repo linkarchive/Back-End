1fcc05e307c43a3bb51d622e3f04ed08040bf7f2
subin <lsb1026@naver.com>
2023-06-02 19:47:15 +0900

    feat: AuthenticationEntryPoint exception message

--- src/main/java/project/linkarchive/backend/security/AuthenticationEntryPointImpl.java
+++ src/main/java/project/linkarchive/backend/security/AuthenticationEntryPointImpl.java
@@ -1,23 +1,30 @@
 package project.linkarchive.backend.security;
 
+import org.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
+@Component
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        authException.printStackTrace();
-        response.setStatus(401);
-        PrintWriter writer = response.getWriter();
-        writer.print("로그인이 필요합니다.");
-        // FIXME: JSON 형식으로 에러 메시지 수정
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json;charset=utf-8");
+        JSONObject json = new JSONObject();
+        String message = "로그인이 필요합니다.";
+        json.put("message", message);
+        PrintWriter out = response.getWriter();
+        out.print(json);
     }
+
 
 }
