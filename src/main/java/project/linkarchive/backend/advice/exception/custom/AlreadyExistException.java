33e5780f00772530fdd2d1cf63cb84b59f5f3086
subin <lsb1026@naver.com>
2023-06-06 12:00:52 +0900

    fix: gettter 어노테이션 삭제

--- src/main/java/project/linkarchive/backend/advice/exception/custom/AlreadyExistException.java
+++ src/main/java/project/linkarchive/backend/advice/exception/custom/AlreadyExistException.java
@@ -1,10 +1,8 @@
 package project.linkarchive.backend.advice.exception.custom;
 
-import lombok.Getter;
 import project.linkarchive.backend.advice.exception.BusinessException;
 import project.linkarchive.backend.advice.exception.ExceptionCodeConst;
 
-@Getter
 public class AlreadyExistException extends BusinessException {
 
     public AlreadyExistException(ExceptionCodeConst exceptionCodeConst) {
