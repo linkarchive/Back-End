c5cf01ac73e4b64dc20966d35d060e455d8ada4a
taegyun1995 <needle12s@gmail.com>
2023-06-25 18:22:47 +0900

    test: use user update nickname request setup date

--- src/test/java/project/linkarchive/backend/user/request/UpdateNicknameRequestTest.java
+++ src/test/java/project/linkarchive/backend/user/request/UpdateNicknameRequestTest.java
@@ -2,19 +2,17 @@ package project.linkarchive.backend.user.request;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import project.linkarchive.backend.util.setUpData.UserSetUpData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static project.linkarchive.backend.util.constant.Constants.NICKNAME;
 
-class UpdateNicknameRequestTest {
+class UpdateNicknameRequestTest extends UserSetUpData {
 
     @DisplayName("UpdateNicknameRequestTest - DTO")
     @Test
-    void testUpdateNicknameRequestTest() {
-        UpdateNicknameRequest updateNicknameRequest = new UpdateNicknameRequest(NICKNAME);
-
-        assertNotNull(updateNicknameRequest);
+    void testUpdateNicknameRequest() {
         assertEquals(NICKNAME, updateNicknameRequest.getNickname());
     }
 
