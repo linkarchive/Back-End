e6389b8bec780a38c4f721c9ff164f726232c320
taegyun1995 <needle12s@gmail.com>
2023-07-02 19:45:10 +0900

    test: user dto test code

--- src/test/java/project/linkarchive/backend/user/request/UpdateNicknameRequestTest.java
+++ src/test/java/project/linkarchive/backend/user/request/UpdateNicknameRequestTest.java
@@ -1,15 +1,19 @@
 package project.linkarchive.backend.user.request;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import project.linkarchive.backend.util.setUpData.UserSetUpData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static project.linkarchive.backend.util.constant.Constants.NEW_NICKNAME;
-import static project.linkarchive.backend.util.constant.Constants.NICKNAME;
 
 class UpdateNicknameRequestTest extends UserSetUpData {
+
+    @BeforeEach
+    public void setup() {
+        setUpUpdateNicknameRequest();
+    }
 
     @DisplayName("UpdateNicknameRequestTest - DTO")
     @Test
--- src/test/java/project/linkarchive/backend/user/response/ProfileResponseTest.java
+++ src/test/java/project/linkarchive/backend/user/response/ProfileResponseTest.java
@@ -1,24 +1,27 @@
 package project.linkarchive.backend.user.response;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import project.linkarchive.backend.util.setUpData.UserSetUpData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static project.linkarchive.backend.util.constant.Constants.*;
 
 class ProfileResponseTest extends UserSetUpData {
 
+    @BeforeEach
+    public void setup() {
+        setUpUser();
+        setUpProfileResponse();
+    }
+
     @DisplayName("ProfileResponse - DTO")
     @Test
     void testProfileResponse() {
-        ProfileResponse profileResponse = new ProfileResponse(user, PROFILE_IMAGE_FILENAME);
-
-        assertNotNull(profileResponse);
         assertEquals(NICKNAME, profileResponse.getNickname());
         assertEquals(INTRODUCE, profileResponse.getIntroduce());
-        assertEquals(PROFILE_IMAGE_FILENAME, profileResponse.getProfileImageFileName());
+        assertEquals(PRE_SIGNED_URL, profileResponse.getProfileImageFileName());
     }
 
 }
