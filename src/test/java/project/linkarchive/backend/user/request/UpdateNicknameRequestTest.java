99da914033413fbec15b29c82249a6e87d2424d0
taegyun1995 <needle12s@gmail.com>
2023-07-20 13:47:29 +0900

    test: implementing user dto test code

--- src/test/java/project/linkarchive/backend/user/request/UpdateNicknameRequestTest.java
+++ src/test/java/project/linkarchive/backend/user/request/UpdateNicknameRequestTest.java
@@ -1,23 +1,19 @@
 package project.linkarchive.backend.user.request;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import project.linkarchive.backend.util.setUpData.UserSetUpData;
+import project.linkarchive.backend.util.setUpData.SetUpMockData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static project.linkarchive.backend.util.constant.Constants.NEW_NICKNAME;
 
-class UpdateNicknameRequestTest extends UserSetUpData {
-
-    @BeforeEach
-    public void setup() {
-        setUpUpdateNicknameRequest();
-    }
+class UpdateNicknameRequestTest extends SetUpMockData {
 
     @DisplayName("UpdateNicknameRequestTest - DTO")
     @Test
     void testUpdateNicknameRequest() {
+        setUpUpdateNicknameRequest();
+
         assertEquals(NEW_NICKNAME, updateNicknameRequest.getNickname());
     }
 
