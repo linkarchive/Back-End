a7b88087f71979f54732ee4e6d3ab2e33c08ceed
taegyun1995 <needle12s@gmail.com>
2023-07-20 13:47:29 +0900

    test: implementing user dto test code

--- src/test/java/project/linkarchive/backend/user/request/UpdateProfileRequestTest.java
+++ src/test/java/project/linkarchive/backend/user/request/UpdateProfileRequestTest.java
@@ -1,24 +1,20 @@
 package project.linkarchive.backend.user.request;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import project.linkarchive.backend.util.setUpData.UserSetUpData;
+import project.linkarchive.backend.util.setUpData.SetUpMockData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static project.linkarchive.backend.util.constant.Constants.NEW_INTRODUCE;
 import static project.linkarchive.backend.util.constant.Constants.NEW_NICKNAME;
 
-class UpdateProfileRequestTest extends UserSetUpData {
-
-    @BeforeEach
-    public void setup() {
-        setUpUpdateProfileRequest();
-    }
+class UpdateProfileRequestTest extends SetUpMockData {
 
     @DisplayName("UpdateProfileResponse - DTO")
     @Test
     void testUpdateProfileResponse() {
+        setUpUpdateProfileRequest();
+
         assertEquals(NEW_NICKNAME, updateProfileRequest.getNickname());
         assertEquals(NEW_INTRODUCE, updateProfileRequest.getIntroduce());
     }
--- src/test/java/project/linkarchive/backend/user/response/ProfileResponseTest.java
+++ src/test/java/project/linkarchive/backend/user/response/ProfileResponseTest.java
@@ -1,25 +1,21 @@
 package project.linkarchive.backend.user.response;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import project.linkarchive.backend.util.setUpData.UserSetUpData;
+import project.linkarchive.backend.util.setUpData.SetUpMockData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static project.linkarchive.backend.util.constant.Constants.EMPTY;
 import static project.linkarchive.backend.util.constant.Constants.PRE_SIGNED_URL;
 
-class ProfileResponseTest extends UserSetUpData {
-
-    @BeforeEach
-    public void setup() {
-        setUpUser();
-        setUpProfileResponse();
-    }
+class ProfileResponseTest extends SetUpMockData {
 
     @DisplayName("ProfileResponse - DTO")
     @Test
     void testProfileResponse() {
+        setUpUser();
+        setUpProfileResponse();
+
         assertEquals(EMPTY, profileResponse.getNickname());
         assertEquals(EMPTY, profileResponse.getIntroduce());
         assertEquals(PRE_SIGNED_URL, profileResponse.getProfileImageFileName());
