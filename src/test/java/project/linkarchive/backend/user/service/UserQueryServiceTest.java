be16c355e37d9aafe2494be6b6fc4c86ca016306
taegyun1995 <needle12s@gmail.com>
2023-07-28 20:07:54 +0900

    test: implementing user query service test code

--- src/test/java/project/linkarchive/backend/user/service/UserQueryServiceTest.java
+++ src/test/java/project/linkarchive/backend/user/service/UserQueryServiceTest.java
@@ -3,6 +3,7 @@ package project.linkarchive.backend.user.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import project.linkarchive.backend.advice.exception.custom.NotFoundException;
 import project.linkarchive.backend.user.response.ProfileResponse;
 import project.linkarchive.backend.util.service.UserSetUpService;
 
@@ -11,7 +12,9 @@ import java.net.URL;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static project.linkarchive.backend.util.constant.Constants.*;
 
@@ -19,9 +22,8 @@ class UserQueryServiceTest extends UserSetUpService {
 
     @BeforeEach
     void setUp() {
-        setUpUser();
-        setUpMultipartFile();
         setUpProfileImage();
+        setUpUser();
     }
 
     @DisplayName("유저 Query Service - getMyProfile")
@@ -31,12 +33,50 @@ class UserQueryServiceTest extends UserSetUpService {
                 .thenReturn(Optional.of(user));
         when(s3Uploader.generatePresignedProfileImageUrl(
                 anyString(),
-                EXPIRATION_TIME_MINUTE)
+                eq(EXPIRATION_TIME_MINUTE))
         ).thenReturn(new URL(PROFILE_IMAGE_URL));
 
         ProfileResponse response = userQueryService.getMyProfile(user.getId());
 
+        validateResponse(response);
+    }
+
+    @DisplayName("유저 Query Service - getMyProfile - User Not Found")
+    @Test
+    void testGetMyProfile_UserNotFound() {
+        when(userRepository.findById(USER_ID))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> userQueryService.getMyProfile(user.getId()));
+    }
+
+    @DisplayName("유저 Query Service - getUserProfile")
+    @Test
+    void testGetUserProfile() throws MalformedURLException {
+        when(userRepository.findByNickname(EMPTY))
+                .thenReturn(Optional.of(user));
+        when(s3Uploader.generatePresignedProfileImageUrl(
+                anyString(),
+                eq(EXPIRATION_TIME_MINUTE))
+        ).thenReturn(new URL(PROFILE_IMAGE_URL));
+
+        ProfileResponse response = userQueryService.getUserProfile(user.getNickname());
+
+        validateResponse(response);
+    }
+
+    @DisplayName("유저 Query Service - getUserProfile - User Not Found")
+    @Test
+    void testGetUserProfile_UserNotFound() {
+        when(userRepository.findByNickname(EMPTY))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> userQueryService.getUserProfile(user.getNickname()));
+    }
+
+    private void validateResponse(ProfileResponse response) {
         assertEquals(user.getId(), response.getId());
+        assertEquals(user.getNickname(), response.getNickname());
         assertEquals(PROFILE_IMAGE_URL, response.getProfileImageFileName());
     }
 
