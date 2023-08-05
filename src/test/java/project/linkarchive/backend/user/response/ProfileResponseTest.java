f34d2cd0b9c73a9bb8db24106d3a41891e8902cb
taegyun1995 <needle12s@gmail.com>
2023-08-05 16:38:37 +0900

    test: implementing user dto test code

--- src/test/java/project/linkarchive/backend/user/response/ProfileResponseTest.java
+++ src/test/java/project/linkarchive/backend/user/response/ProfileResponseTest.java
@@ -1,24 +1,30 @@
 package project.linkarchive.backend.user.response;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import project.linkarchive.backend.util.setUpData.SetUpMockData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static project.linkarchive.backend.util.constant.Constants.EMPTY;
-import static project.linkarchive.backend.util.constant.Constants.PRE_SIGNED_URL;
+import static project.linkarchive.backend.util.constant.Constants.*;
 
 class ProfileResponseTest extends SetUpMockData {
+
+    @BeforeEach
+    void setUp() {
+        setUpUser();
+        setUpProfileResponse();
+    }
 
     @DisplayName("ProfileResponse - DTO")
     @Test
     void testProfileResponse() {
-        setUpUser();
-        setUpProfileResponse();
-
+        assertEquals(USER_ID, profileResponse.getId());
         assertEquals(EMPTY, profileResponse.getNickname());
         assertEquals(EMPTY, profileResponse.getIntroduce());
         assertEquals(PRE_SIGNED_URL, profileResponse.getProfileImageFileName());
+        assertEquals(FOLLOWER_COUNT, profileResponse.getFollowerCount());
+        assertEquals(FOLLOWING_COUNT, profileResponse.getFollowingCount());
     }
 
 }
--- src/test/java/project/linkarchive/backend/user/response/UpdateNicknameResponseTest.java
+++ src/test/java/project/linkarchive/backend/user/response/UpdateNicknameResponseTest.java
@@ -1,24 +1,32 @@
 package project.linkarchive.backend.user.response;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import project.linkarchive.backend.util.setUpData.SetUpMockData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static project.linkarchive.backend.util.constant.Constants.NEW_NICKNAME;
 
 class UpdateNicknameResponseTest extends SetUpMockData {
 
+    @BeforeEach
+    void setUp() {
+        setUpUser();
+        setUpUpdateNicknameRequest();
+    }
+
     @DisplayName("UpdateNicknameResponse - DTO")
     @Test
     void testUpdateNicknameResponse() {
-        setUpUser();
-        setUpUpdateNicknameRequest();
+        String oldNickname = user.getNickname();
         user.updateNickName(updateNicknameRequest);
 
         setUpUpdateNicknameResponse();
         String newNickname = updateNicknameResponse.getNickname();
 
+        assertNotEquals(oldNickname, newNickname);
         assertEquals(NEW_NICKNAME, newNickname);
     }
 
