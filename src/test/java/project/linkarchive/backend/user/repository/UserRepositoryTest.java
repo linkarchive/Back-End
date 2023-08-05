dee284d35052741a739c29a745998d6a27f3f62a
taegyun1995 <needle12s@gmail.com>
2023-08-05 17:04:31 +0900

    test: implementing user repository test code

--- src/test/java/project/linkarchive/backend/user/repository/UserRepositoryTest.java
+++ src/test/java/project/linkarchive/backend/user/repository/UserRepositoryTest.java
@@ -4,24 +4,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import project.linkarchive.backend.advice.exception.custom.NotFoundException;
-import project.linkarchive.backend.util.repository.UserSetUpRepository;
+import project.linkarchive.backend.util.setUpData.SetUpMockData;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static project.linkarchive.backend.advice.exception.ExceptionCodeConst.NOT_FOUND_USER;
 import static project.linkarchive.backend.util.constant.Constants.*;
 
-class UserRepositoryTest extends UserSetUpRepository {
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest extends SetUpMockData {
+
+    @Autowired
+    protected UserRepository userRepository;
 
     @BeforeEach
     void repositorySetup() {
         setUpUser();
-        userRepository.save(user);
     }
 
     @DisplayName("유저 Repository - findBySocialId")
     @Test
     void testFindBySocialId() {
+        userRepository.save(user);
+
         user = userRepository.findBySocialId(SOCIAL_ID)
                 .orElseThrow(() -> new NotFoundException(NOT_FOUND_USER));
 
@@ -44,6 +53,8 @@ class UserRepositoryTest extends UserSetUpRepository {
     @DisplayName("유저 Repository - existsUserByNickname")
     @Test
     void testExistsUserByNickname() {
+        userRepository.save(user);
+
         Boolean exist = userRepository.existsUserByNickname(user.getNickname());
 
         Assertions.assertTrue(exist, user.getNickname());
@@ -60,6 +71,8 @@ class UserRepositoryTest extends UserSetUpRepository {
     @DisplayName("유저 Repository - findByNickname")
     @Test
     void testFindByNickname() {
+        userRepository.save(user);
+
         user = userRepository.findByNickname(EMPTY)
                 .orElseThrow(() -> new NotFoundException(NOT_FOUND_USER));
 
